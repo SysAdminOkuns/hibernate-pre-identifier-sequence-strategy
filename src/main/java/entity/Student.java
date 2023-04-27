1485b0100764ce5678917e8e8cba3f5454100e39
SysAdminOkuns <sysad.okundaye@gmail.com>
2023-04-27 12:00:37 -0400

    Working with hibernate_sequence with pre-insert identifier for optimization of insert operation

---
+++ src/main/java/entity/Student.java
@@ -0,0 +1,38 @@
+package entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
+
+@Entity
+public class Student {
+	
+	@Id
+//	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private Long id;
+	
+	@Column(name="enrollment_id", nullable=false, unique=true)
+	private String enrollmentId;	
+	
+	private String name;
+
+	public Student() {}
+	public Student(String enrollmentId, String name) {
+		this.enrollmentId = enrollmentId;
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", enrollmentId=" + enrollmentId + ", name=" + name + "]";
+	}	
+	
+}
